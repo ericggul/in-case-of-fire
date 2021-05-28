028ac05d8d02ac094175ac67b4b3b0fe44f15229
ericggul <57379903+ericggul@users.noreply.github.com>
2021-05-28 20:49:52 +0900

    Oringinal Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+function Main({color}) {
+
+  var i = parseInt(color.toString(16),16);
+  if(i<10){
+    i = `0${i}`
+  }
+  var derivedColor = `#${i}${i}${i}`;
+
+  return(
+    <div className="main" style={{ backgroundColor: derivedColor}}>
+      In case of fire use stairs unless otherwise instructed
+    </div>
+  )
+}
+
+
+
 function App() {
+  function randomHex(){
+    return parseInt(Math.floor(Math.random() * 60 + 20).toString(16),16);
+  }
+  const sxtnarray = new Array(100).fill(randomHex());
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {sxtnarray.map((j,i)=>(
+        <Main key={i} color={i} />
+      ))}
     </div>
   );
 }
